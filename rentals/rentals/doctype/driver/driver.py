e48e74ee29ff519780306409650ac47a914c7096
md4648 <mulunehdebebe66@gmail.com>
2025-03-15 12:58:29 +0300

    first commit

---
+++ rentals/rentals/doctype/driver/driver.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025, md and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Driver(Document):
+	pass
+
+
+# API Secret: 90c6be1f943a8e3
+# API Key: b31b3424a77cd63
